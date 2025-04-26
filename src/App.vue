c5b9171ee9f7c50c44df2fd92203357a9c329f36
facronactz <farroaxza@gmail.com>
2025-04-27 02:19:38 +0700

    refactor(src): remove unused components and update App layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Home from "./components/Home.vue";
-import Merger from "./components/Merger.vue";
+import Merger from "./pages/Merger.vue";
 </script>
 
 <template>
-<!--  <Home />-->
-<!--  <HelloWorld msg="Vite + Vue" />-->
   <Merger/>
 </template>
 
